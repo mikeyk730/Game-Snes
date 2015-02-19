ac571981b82ea3ef32feea02306eac67afae7322
unknown <mikeyk730@gmail.com>
2015-02-19 14:12:24 +0200

    update driver files to match public all.log

---
+++ disasm/driver_files/all/all.bat
@@ -0,0 +1,8 @@
+cat ..\bank*.sym      | sort  1>all.sym 
+cat ..\bank*.data     | sort  1>all.data 
+cat ..\bank*.flags    | sort  1>all.flags 
+cat ..\bank*.trace    | sort  1>all.trace 
+cat ..\bank*.code     | sort  1>all.code 
+cat ..\bank*.comment          1>all.comment 
+
+echo 8000 100000 -e | disasm.exe --ram all.ram --sym all.sym --ptr all.ptr --data all.data --accum all.flags --comment all.comment --sym2 all.trace mario.smc 1> all.log 2>null
